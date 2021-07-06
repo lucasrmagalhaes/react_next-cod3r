afbc6c7b0d1f3a36ba08a08e7adec1119b52c43f
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-06 19:02:28 -0300

    Propriedades de Componente

--- exercicios/components/Titulo.jsx
+++ exercicios/components/Titulo.jsx
@@ -1,8 +1,15 @@
-export default function Titulo() {
-    return (
+export default function Titulo(props) {
+    console.log(props)
+
+    return props.pequeno ? (
         <>
-            <h1>Título</h1>
-            <h2>Subtítulo</h2>
+            <p>{props.principal}</p>
+            <p>{props.secundario}</p>
+        </>
+    ) : (
+        <>
+            <h1>{props.principal}</h1>
+            <h2>{props.secundario}</h2>
         </>
     )
 }
--- exercicios/pages/basicos/usandoTitulo.jsx
+++ exercicios/pages/basicos/usandoTitulo.jsx
@@ -3,7 +3,25 @@ import Titulo from "../../components/Titulo"
 export default function usandoTitulo() {
     return (
         <div>
-            <Titulo />
+            <Titulo 
+                principal="Página de Cadastro" 
+                secundario="Incluir, alterar e excluir coisas!"
+            />
+            <Titulo 
+                principal="Página de Cadastro" 
+                secundario="Incluir, alterar e excluir coisas!"
+                pequeno={false}
+            />
+            <Titulo 
+                principal="Página de Login" 
+                secundario="Informe o seu e-mail e senha." 
+                pequeno={true}
+            />
+            <Titulo
+                principal="Página de Login" 
+                secundario="Informe o seu e-mail e senha." 
+                pequeno
+            />
         </div>
     )
 }
