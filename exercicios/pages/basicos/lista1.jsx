ed4738f620c182e8086e02067d5396dc2fe0b574
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-06 17:56:50 -0300

    Desafio Lista - Resposta 01

--- exercicios/pages/basicos/lista1.jsx
+++ exercicios/pages/basicos/lista1.jsx
@@ -1,14 +1,16 @@
-/*
-    <div>
-        <span>1,</span>
-        <span>2,</span>
-        <span>3,</span>
-        <span>4,</span>
-        <span>5,</span>
-        <span>6,</span>
-        <span>7,</span>
-        <span>8,</span>
-        <span>9,</span>
-        <span>10,</span>
-    </div>
-*/
+export default function lista() {
+    return (
+        <div>
+            <span>1,</span>
+            <span>2,</span>
+            <span>3,</span>
+            <span>4,</span>
+            <span>5,</span>
+            <span>6,</span>
+            <span>7,</span>
+            <span>8,</span>
+            <span>9,</span>
+            <span>10,</span>
+        </div>
+    )
+}
