d1d15d5c9a01df4ccccd7e440d480893c775d863
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-06 21:28:54 -0300

    Projeto Tabuleiro 03

---
+++ tabuleiro/components/Subdivisao.jsx
@@ -0,0 +1,7 @@
+import styles from '../styles/Subdivisao.module.css'
+
+export default function Subdivisao(props) {
+    return (
+        <div className={styles.subdivisao}></div>
+    )
+}
---
+++ tabuleiro/pages/index.jsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css'
+import Subdivisao from '../components/Subdivisao'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Subdivisao />
+    </div>
+  )
+}
