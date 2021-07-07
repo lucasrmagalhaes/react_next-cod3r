fa0376feaf48b71f86d09af9337a7e982da2a99d
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-06 22:05:49 -0300

    Projeto Tabuleiro 05

--- tabuleiro/components/Linha.jsx
+++ tabuleiro/components/Linha.jsx
@@ -4,14 +4,14 @@ import styles from '../styles/Linha.module.css'
 export default function Linha(props) {
     return (
         <div className={styles.linha}>
-            <Subdivisao />
-            <Subdivisao preta />
-            <Subdivisao />
-            <Subdivisao preta />
-            <Subdivisao />
-            <Subdivisao preta />
-            <Subdivisao />
-            <Subdivisao preta />
+            <Subdivisao preta={props.preta} />
+            <Subdivisao preta={!props.preta} />
+            <Subdivisao preta={props.preta} />
+            <Subdivisao preta={!props.preta} />
+            <Subdivisao preta={props.preta} />
+            <Subdivisao preta={!props.preta} />
+            <Subdivisao preta={props.preta} />
+            <Subdivisao preta={!props.preta} />
         </div>
     )
 }
--- tabuleiro/pages/index.jsx
+++ tabuleiro/pages/index.jsx
@@ -1,11 +1,10 @@
 import styles from '../styles/Home.module.css'
-import Linha from '../components/Linha'
+import Tabuleiro from '../components/Tabuleiro'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Linha />
-      <Linha />
+      <Tabuleiro />
     </div>
   )
 }
