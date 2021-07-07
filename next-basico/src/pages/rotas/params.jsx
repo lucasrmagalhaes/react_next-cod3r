ff0c287d9bef0f54725ee1a362d9473536ec8e86
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-07 15:01:37 -0300

    Rotas 03

---
+++ next-basico/src/pages/rotas/params.jsx
@@ -0,0 +1,15 @@
+import { useRouter } from 'next/router'
+
+export default function params() {
+    const router = useRouter()
+    // console.log(router)
+    const id = router.query.id
+    const nome = router.query.nome
+
+    return (
+        <div>
+            {/* http://localhost:3000/rotas/params?id=123&nome=Lucas */}
+            <h1>Rotas Params: {id} e {nome}</h1>
+        </div>
+    )
+}
