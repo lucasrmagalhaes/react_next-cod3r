df25b38697523bfe09a3cb99bc3ae860d119e2ba
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-07 08:59:04 -0300

    Comunicação Direta

---
+++ exercicios/pages/comunicacao/direta.jsx
@@ -0,0 +1,9 @@
+import Pai from "../../components/direta/Pai"
+
+export default function direta() {
+    return (
+        <div>
+            <Pai familia="Silva" nome="Padrão" />
+        </div>
+    )
+}
