6b9368b25180014bdcda2c213b6d6ed0cf521180
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-07 14:16:40 -0300

    Rotas 02

---
+++ next-basico/src/pages/rotas/[codigo]/buscar.jsx
@@ -0,0 +1,13 @@
+import { useRouter } from 'next/router'
+
+export default function buscar() {
+    const router = useRouter()
+    // console.log(router)
+    const codigo = router.query.codigo
+    
+    return (
+        <div>
+            <h1>Rotas  / {codigo} / Buscar!</h1>
+        </div>
+    )
+}
