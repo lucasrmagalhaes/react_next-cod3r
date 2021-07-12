be176993d4d1905191cde1158b010848e28ac7a7
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-12 14:46:24 -0300

    Esquema de Cores

--- admin-template/src/components/template/Layout.tsx
+++ admin-template/src/components/template/Layout.tsx
@@ -13,7 +13,7 @@ export default function Layout(props: LayoutProps) {
     <div className={`flex h-screen w-screen`}>
       <MenuLateral />
       <div className={`
-        flex flex-col w-full p-7 bg-gray-300`
+        flex flex-col w-full p-7 bg-gray-300 dark:bg-gray-800` 
       }>
         <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
         <Conteudo>
--- admin-template/tailwind.config.js
+++ admin-template/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}'
   ],
-  darkMode: false,
+  darkMode: 'class',
   theme: {
     extend: {},
   },
