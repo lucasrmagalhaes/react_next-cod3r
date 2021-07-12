1269b3f3876f523dd16edf521a01062014e29bd8
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-12 15:17:43 -0300

    Componente Logo

--- admin-template/src/components/template/MenuLateral.tsx
+++ admin-template/src/components/template/MenuLateral.tsx
@@ -1,9 +1,17 @@
 import { IconeAjustes, IconeCasa, IconeSino } from "../icons"
+import Logo from "./Logo"
 import MenuItem from "./MenuItem"
 
 export default function MenuLateral() {
   return (
     <aside>
+      <div className={`
+        flex flex-col items-center justify-center
+        bg-gradient-to-r from-indigo-500 to-purple-800
+        h-20 w-20
+      `}>
+        <Logo />
+      </div>
       <ul>
         <MenuItem url="/" texto="Início" icone={IconeCasa} />
         <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
