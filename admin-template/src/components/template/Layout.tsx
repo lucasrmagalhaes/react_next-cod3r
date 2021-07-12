9a6b549922f337374d40d598b0f65b32ee9003f1
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-12 14:35:41 -0300

    Componente Layout 01

--- admin-template/src/components/template/Layout.tsx
+++ admin-template/src/components/template/Layout.tsx
@@ -3,19 +3,23 @@ import Cabecalho from "./Cabecalho";
 import Conteudo from "./Conteudo";
 
 interface LayoutProps {
-    titulo: string;
-    subtitulo: string;
-    children?: any;
+  titulo: string;
+  subtitulo: string;
+  children?: any;
 }
 
 export default function Layout(props: LayoutProps) {
-    return (
-        <div>
-            <MenuLateral />
-            <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
-            <Conteudo>
-                {props.children}
-            </Conteudo>
-        </div>
-    )
+  return (
+    <div className={`flex h-screen w-screen`}>
+      <MenuLateral />
+      <div className={`
+        flex flex-col w-full p-7 bg-gray-300`
+      }>
+        <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
+        <Conteudo>
+          {props.children}
+        </Conteudo>
+      </div>
+    </div>
+  )
 }
