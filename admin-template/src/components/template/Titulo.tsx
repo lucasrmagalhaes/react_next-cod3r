aa7552a0f1902f9c6479d3a5a926076a10f3df7f
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-12 15:09:47 -0300

    Componente Título

--- admin-template/src/components/template/Titulo.tsx
+++ admin-template/src/components/template/Titulo.tsx
@@ -7,10 +7,11 @@ export default function Layout(props: TituloProps) {
     return (
         <div>
             <h1 className={`
-
+                font-black text-3xl
+                text-gray-900
             `}>{props.titulo}</h1>
             <h2 className={`
-            
+                font-light text-sm text-gray-600
             `}>{props.subtitulo}</h2>
         </div>
     )
