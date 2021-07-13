c4e6217dcbc0f9e8e3f4a27610ed63556a8ff087
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-13 08:38:31 -0300

    Tela Autenticação 04

--- admin-template/src/pages/autenticacao.tsx
+++ admin-template/src/pages/autenticacao.tsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 import AuthInput from "../components/auth/AuthInput";
+import { IconeAtencao } from "../components/icons";
+
 
 export default function Autenticacao() {
     const [modo, setModo] = useState<'login' | 'cadastro'>('login') 
     const [email, setEmail] = useState('')
     const [senha, setSenha] = useState('')
+    const [erro, setErro] = useState(null)
+
+    function exibirErro(msg, tempoEmSegundos = 5) {
+        setErro(msg)
+        setTimeout(() => setErro(null), tempoEmSegundos * 1000)
+    }
 
     function submeter() {
         if (modo === 'login') {
@@ -29,6 +37,18 @@ export default function Autenticacao() {
                 `}>
                     {modo === 'login' ? 'Entre com a sua conta' : 'Cadastre-se na plataforma'}
                 </h1>
+
+                {erro ? (
+                    <div className={`
+                        flex items-center
+                        bg-red-400 text-white py-3 px-5 my-2
+                        border border-red-700 rounded-lg
+                    `}>
+                        {IconeAtencao}
+                        <span className={"ml-3"}>{erro}</span>
+                    </div>
+                ) : false}
+
                 <AuthInput 
                     label="Email"
                     tipo="email"
