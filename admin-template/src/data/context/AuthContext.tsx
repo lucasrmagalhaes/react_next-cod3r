049f08b4ca20eeb50629a80dffbd6b932c164940
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-13 09:50:04 -0300

    Autenticação com Google

--- admin-template/src/data/context/AuthContext.tsx
+++ admin-template/src/data/context/AuthContext.tsx
@@ -11,24 +11,31 @@ interface AuthContextProps {
 
 const AuthContext = createContext<AuthContextProps>({})
 
-// async function usuarioNormalizado(usuarioFirebase: firebase.User): Promise<Usuario> {
-//     const token = await usuarioFirebase.getIdToken()
-//     return {
-//         uid: usuarioFirebase.uid,
-//         nome: usuarioFirebase.displayName,
-//         email: usuarioFirebase.email,
-//         token,
-//         provedor: usuarioFirebase.providerData[0].providerId,
-//         imagemUrl: usuarioFirebase.photoURL
-//     }
-// }
+async function usuarioNormalizado(usuarioFirebase: firebase.User): Promise<Usuario> {
+    const token = await usuarioFirebase.getIdToken()
+    return {
+        uid: usuarioFirebase.uid,
+        nome: usuarioFirebase.displayName,
+        email: usuarioFirebase.email,
+        token,
+        provedor: usuarioFirebase.providerData[0].providerId,
+        imagemUrl: usuarioFirebase.photoURL
+    }
+}
 
 export function AuthProvider(props) {
     const [usuario, setUsuario] = useState<Usuario>(null)
 
     async function loginGoogle() {
-        console.log('Login Google...')
-        router.push('/')
+        const resp = await firebase.auth().signInWithPopup(
+            new firebase.auth.GoogleAuthProvider()
+        )
+
+        if (resp.user?.email) {
+            const usuario = await usuarioNormalizado(resp.user)
+            setUsuario(usuario)
+            router.push('/')
+        }
     }
 
     return (
