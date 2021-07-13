52f0c9971f77975d02e1f4d22c860a25e0cf2f3f
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-13 10:14:52 -0300

    Avatar Usuário

--- admin-template/src/components/template/Cabecalho.tsx
+++ admin-template/src/components/template/Cabecalho.tsx
@@ -1,4 +1,5 @@
 import useAppData from "../../data/hook/useAppData";
+import AvatarUsuario from "./AvatarUsuario";
 import BotaoAlternarTema from "./BotaoAlternarTema";
 import Titulo from "./Titulo";
 
@@ -13,8 +14,9 @@ export default function Layout(props: CabecalhoProps) {
     return (
         <div className={`flex`}>
             <Titulo titulo={props.titulo} subtitulo={props.subtitulo} />
-            <div className={`flex flex-grow justify-end`}>
+            <div className={`flex flex-grow justify-end items-center`}>
                 <BotaoAlternarTema tema={tema} alternarTema={alternarTema} />
+                <AvatarUsuario className="ml-3" />
             </div>
         </div>
     )
