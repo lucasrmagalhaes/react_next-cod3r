dd1cedd15af93c42551acb7b51717897a97654bb
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-23 17:09:17 -0300

    Componente Porta 06

--- components/Porta.jsx
+++ components/Porta.jsx
@@ -1,15 +1,17 @@
 import styles from "../styles/Porta.module.css"
 
-export default function Porta(props) {
-    return (
-        <div className={styles.area}>
-            <div className={styles.estrutura}>
-                <div className={styles.porta}>
-                    <div className={styles.numero}>3</div>
-                    <div className={styles.macaneta}></div>
-                </div>
-            </div>
-            <div className={styles.chao}></div>
+export default function Porta({ selecionada }) {
+  const portaSelecionada = selecionada ? styles.selecionada : ''
+
+  return (
+    <div className={styles.area}>
+      <div className={`${styles.estrutura} ${portaSelecionada}`}>
+        <div className={styles.porta}>
+          <div className={styles.numero}>3</div>
+          <div className={styles.macaneta}></div>
         </div>
-    )
+      </div>
+      <div className={styles.chao}></div>
+    </div>
+  )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,9 +3,8 @@ import Porta from "../components/Porta"
 export default function Home() {
   return (
     <div style={{ display: 'flex' }}>
-      <Porta />
-      <Porta />
-      <Porta />
+      <Porta selecionada />
+      <Porta  />
     </div>
   )
 }
