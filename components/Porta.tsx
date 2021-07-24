6347503fdd731e946b634404d69f7e12c4657966
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-24 19:53:04 -0300

    Adicionando Eventos a Porta 03

--- components/Porta.tsx
+++ components/Porta.tsx
@@ -2,24 +2,39 @@ import styles from "../styles/Porta.module.css"
 import PortaModel from "../model/porta"
 
 interface PortaProps {
-  value: PortaModel
+  porta: PortaModel
   onChange: (novaPorta: PortaModel) => void
 }
 
-export default function Porta({ value, onChange }: PortaProps) {
-  const selecionada = value.selecionada ? styles.selecionada : ''
+export default function Porta({ porta, onChange }: PortaProps) {
+  const selecionada = porta.selecionada && !porta.aberta ? styles.selecionada : ''
 
-  const alternarSelecao = e => onChange(value.alternarSelecao())
+  const alternarSelecao = e => onChange(porta.alternarSelecao())
+  
+  const abrir = e => {
+    e.stopPropagation()
+    onChange(porta.abrir())
+  }
+
+  function renderizarPorta() {
+    return (
+      <div className={styles.porta}>
+        <div className={styles.numero}>{porta.numero}</div>
+        <div 
+          className={styles.macaneta}
+          onClick={abrir}
+        />
+      </div>
+    )
+  }
 
   return (
     <div className={styles.area} onClick={alternarSelecao}>
-      <div className={`${styles.estrutura} ${selecionada}`}>
-        <div className={styles.porta}>
-          <div className={styles.numero}>{value.numero}</div>
-          <div className={styles.macaneta}></div>
+       <div className={`${styles.estrutura} ${selecionada}`}>
+          { porta.aberta ? false : renderizarPorta() }
         </div>
-      </div>
-      <div className={styles.chao}></div>
+
+        <div className={styles.chao}></div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,10 @@ import PortaModel from "../model/porta"
 
 export default function Home() {
   const [p1, setP1] = useState(new PortaModel(1))
-  const [texto, setTexto] = useState("...")
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <input 
-        type="text" 
-        value={texto} 
-        onChange={e => setTexto(e.target.value)} 
-      />
-      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)} />
+      <Porta porta={p1} onChange={novaPorta => setP1(novaPorta)} />
     </div>
   )
 }
