fc92a2b1518e2c5b7b48f1f7ecbb1689d53ed6cf
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-24 20:21:34 -0300

    Lidando com Array de Portas 02

--- functions/portas.ts
+++ functions/portas.ts
@@ -7,3 +7,15 @@ export function criarPortas(qtde: number, selecionada: number): PortaModel[] {
     return new PortaModel(numero, temPresente)
   })
 }
+
+export function atualizarPortas(portas: PortaModel[], portaModificada: PortaModel): PortaModel[] {
+  return portas.map(portaAtual => {
+    const igualAModificada = portaAtual.numero === portaModificada.numero
+
+    if (igualAModificada) {
+      return portaModificada
+    } else {
+      return portaModificada.aberta ? portaAtual : portaAtual.desselecionar()
+    }
+  })
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,26 @@
 import { useState } from "react"
 import Porta from "../components/Porta"
-import { criarPortas } from "../functions/portas"
+import { atualizarPortas, criarPortas } from "../functions/portas"
 import PortaModel from "../model/porta"
 
 export default function Home() {
-  const [p1, setP1] = useState(new PortaModel(1))
+  const [portas, setPortas] = useState(criarPortas(3, 2))
 
-  console.log(criarPortas(3, 2))
+  function renderizarPortas() {
+    return portas.map(porta => {
+      return (
+        <Porta 
+          key={porta.numero} 
+          porta={porta} 
+          onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))} 
+        />
+      )
+    })
+  }
 
   return (
     <div style={{ display: 'flex' }}>
-      <Porta porta={p1} onChange={novaPorta => setP1(novaPorta)} />
+      {renderizarPortas()}
     </div>
   )
 }
