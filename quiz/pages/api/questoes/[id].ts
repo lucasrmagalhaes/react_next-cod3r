b32e3f3f3457c42959d7f0e67fbb5124541130c6
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-27 21:25:26 -0300

    Construindo a API 01

--- quiz/pages/api/questoes/[id].ts
+++ quiz/pages/api/questoes/[id].ts
@@ -1,5 +1,14 @@
 import questoes from "../bancoDeQuestoes"
 
 export default (req, res) => {
-  res.status(200).json(questoes[0].converterParaObjeto())
+  const idSelecionado = +req.query.id
+
+  const unicaQuestaoOuNada = questoes.filter(questao => questao.id === idSelecionado)
+
+  if (unicaQuestaoOuNada.length === 1) {
+    const questaoSelecionada = unicaQuestaoOuNada[0]
+    res.status(200).json(questaoSelecionada.converterParaObjeto())
+  } else {
+    res.status(204).send() // 204 - No Content
+  }
 }
