6889dc90a65ad1c8b4a4d030f84f4414fcacefb0
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-09-17 11:39:31 -0300

    Componente Index 03

--- quiz/pages/index.tsx
+++ quiz/pages/index.tsx
@@ -1,21 +1,16 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import Questionario from '../components/Questionario';
 import QuestaoModel from '../model/questao'
-import RespostaModel from '../model/resposta'
-
-const questaoMock = new QuestaoModel(1, 'Qual é a melhor cor?', [
-  RespostaModel.errada('Verde'),
-  RespostaModel.errada('Vermelha'),
-  RespostaModel.errada('Azul'),
-  RespostaModel.certa('Preta'),
-])
 
 const BASE_URL = 'http://localhost:3000/api'
 
 export default function Home() {
+  const router = useRouter();
+
   const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([]);
-  const [questao, setQuestao] = useState<QuestaoModel>(questaoMock);
+  const [questao, setQuestao] = useState<QuestaoModel>();
   const [respostasCertas, setRespostasCertas] = useState<number>(0);
 
   async function carregarIdsDasQuestoes() {
@@ -48,8 +43,30 @@ export default function Home() {
     // console.log(respostasCertas + (acertou ? 1 : 0));
   }
 
-  function irPraProximoPasso() {
+  function idProximaPergunta() {
+    if (questao) {
+      const proximoIndice = idsDasQuestoes.indexOf(questao.id) + 1;
+      return idsDasQuestoes[proximoIndice];
+    }
+  }
 
+  function irPraProximoPasso() {
+    const proximoId = idProximaPergunta();
+    proximoId ? irPraProximaQuestao(proximoId) : finalizar();
+  }
+
+  function irPraProximaQuestao(proximoId: number) {
+    carregarQuestao(proximoId);
+  }
+
+  function finalizar() {
+    router.push({
+      pathname: '/resultado',
+      query: {
+        perguntas: idsDasQuestoes.length,
+        certas: respostasCertas,
+      }
+    });
   }
 
   return (
@@ -59,7 +76,7 @@ export default function Home() {
       </Head>
       <Questionario 
         questao={questao}
-        ultima={false}
+        ultima={idProximaPergunta() === undefined}
         questaoRespondida={questaoRespondida}
         irPraProximoPasso={irPraProximoPasso}
       />
