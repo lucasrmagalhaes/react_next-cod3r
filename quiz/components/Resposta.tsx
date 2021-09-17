58a9ef22555869a2bb93567f5fc59c0dffce7d60
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-09-17 14:12:46 -0300

    Animação Resposta

--- quiz/components/Resposta.tsx
+++ quiz/components/Resposta.tsx
@@ -11,41 +11,39 @@ interface RespostaProps {
 }
 
 export default function Resposta({ valor, indice, letra, corFundoLetra, respostaFornecida }: RespostaProps) {
-  const resposta = valor
+  const resposta = valor;
+  const respostaReveleada = resposta.revelada ? styles.respostaReveleada : '';
 
   return (
     <div 
       className={styles.resposta}
       onClick={() => respostaFornecida(indice)}
     >
-      <div className={styles.conteudoResposta}>
-        {!resposta.revelada ? (
-          <div className={styles.frente}>
-            <div 
-              className={styles.letra}
-              style={{backgroundColor: corFundoLetra}}
-            >
-              {letra}
-            </div>
-            <div className={styles.valor}>
-              {resposta.valor}
-            </div>
+      <div className={`${respostaReveleada} ${styles.conteudoResposta}`}>
+        <div className={styles.frente}>
+          <div 
+            className={styles.letra}
+            style={{backgroundColor: corFundoLetra}}
+          >
+            {letra}
           </div>
-        ) : (
-          <div className={styles.verso}>
-            {resposta.certa ? (
-              <div className={styles.certa}>
-                <div>A resposta certa é...</div>
-                <div className={styles.valor}>{resposta.valor}</div>
-              </div>
-            ) : (
-              <div className={styles.errada}>
-                <div>A resposta informada está errada...</div>
-                <div className={styles.valor}>{resposta.valor}</div>
-              </div>
-            )}
+          <div className={styles.valor}>
+            {resposta.valor}
           </div>
-        )}
+        </div>
+        <div className={styles.verso}>
+          {resposta.certa ? (
+            <div className={styles.certa}>
+              <div>A resposta certa é...</div>
+              <div className={styles.valor}>{resposta.valor}</div>
+            </div>
+          ) : (
+            <div className={styles.errada}>
+              <div>A resposta informada está errada...</div>
+              <div className={styles.valor}>{resposta.valor}</div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   )
