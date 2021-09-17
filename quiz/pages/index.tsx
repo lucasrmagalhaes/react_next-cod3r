c11773bda58008cf3d9e3d3d6db42448e0485537
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-09-17 11:09:50 -0300

    Componente Index 01

--- quiz/pages/index.tsx
+++ quiz/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Questionario from '../components/Questionario';
 import QuestaoModel from '../model/questao'
 import RespostaModel from '../model/resposta'
@@ -10,8 +10,32 @@ const questaoMock = new QuestaoModel(1, 'Qual é a melhor cor?', [
   RespostaModel.certa('Preta'),
 ])
 
+const BASE_URL = 'http://localhost:3000/api'
+
 export default function Home() {
-  const [questao, setQuestao] = useState(questaoMock);
+  const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([]);
+  const [questao, setQuestao] = useState<QuestaoModel>(questaoMock);
+
+  async function carregarIdsDasQuestoes() {
+    const resp = await fetch(`${BASE_URL}/questionario`);
+    const idsDasQuestoes = await resp.json();
+    console.log(idsDasQuestoes);
+    setIdsDasQuestoes(idsDasQuestoes);
+  }
+
+  async function carregarQuestao(idQuestao: number) {
+    const resp = await fetch(`${BASE_URL}/questoes/${idQuestao}`);
+    const json = await resp.json();
+    console.log(json);
+  }
+
+  useEffect(() => {
+    carregarIdsDasQuestoes();
+  }, [])
+
+  useEffect(() => {
+    idsDasQuestoes.length > 0 && carregarQuestao(idsDasQuestoes[0]);
+  }, [idsDasQuestoes])
 
   function questaoRespondida(questao: QuestaoModel) {
 
@@ -22,19 +46,11 @@ export default function Home() {
   }
 
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh',
-    }}>
-      <Questionario 
-        questao={questao}
-        ultima={false}
-        questaoRespondida={questaoRespondida}
-        irPraProximoPasso={irPraProximoPasso}
-      />      
-    </div>
+    <Questionario 
+      questao={questao}
+      ultima={false}
+      questaoRespondida={questaoRespondida}
+      irPraProximoPasso={irPraProximoPasso}
+    />      
   )
 }
